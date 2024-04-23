be7c55379834bc14374e5a363352ac9d1d5bb64a
Tejashri Github <tsshinde357@gmail.com>
2024-04-24 00:49:56 +0530

    Login Page added

--- app.js
+++ app.js
@@ -14,10 +14,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get("/",(req,res)=>{
     res.send("Hi I am root");
 });
+app.get("/login", async (req, res) => {
+    console.log("Hello from Login");
+    const currentDate = new Date();
+    const formattedDate = formatDate(currentDate);
+    res.render("./payrolls/index.ejs",{formattedDate});
+});
 app.get("/Home", async (req, res) => {
     console.log("Hello from Home")
-    res.render("./payrolls/index.ejs");
+    res.render("./payrolls/home.ejs");
 });
 app.listen(8080,()=>{
     console.log("Server listening at port 8080");
 });
+
+function formatDate(date) {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const day = days[date.getDay()];
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    let hours = date.getHours();
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    const meridiem = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12; // Convert to 12-hour format
+    return `${day}, ${month}, ${year} | ${hours}:${minutes}:${seconds}:${meridiem}`;
+  }
